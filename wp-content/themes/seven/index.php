8c23a77324215b5ea4a8eff62189ab2504d68e4c
JeelKumar Patel <jeelhpatel10@gmail.com>
2023-09-07 15:40:25 -0400

    the loop
    using the famous loop in wordpress to display the content of the block posts in wordpress

--- wp-content/themes/seven/index.php
+++ wp-content/themes/seven/index.php
@@ -1,7 +1,18 @@
 <?php
-
+    /*
+    The Loop
+    we can use have_posts() to see if we have any post
+    then we can do the following,
+    the_post();
+    then the_title() to get the title of the post
+    then the_content() to get the content of the post
+    
+    we can use the function the_permalink() to get the url for the block post individual/detail page
+    */
+    while(have_posts()){
+        the_post(); ?>
+        <h2><a href="<?php the_permalink(); ?>"><?php the_title() ?></a></h2>
+        <p><?php the_content() ?></p>
+        <hr>
+    <?php }
 ?>
-
-<!-- blofinfo() provides us all the information about our website -->
-<h1><?php bloginfo("name"); ?></h1>
-<p><?php bloginfo("description"); ?></p>
