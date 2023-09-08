2daa55db10ba0c07b13c05d15ea7e62cb6deaa0f
JeelKumar Patel <jeelhpatel10@gmail.com>
2023-09-08 16:58:06 -0400

    side navigation
    used wp_list_pages() to get the list of all the pages, used more of the parent child page functions.

--- wp-content/themes/seven/page.php
+++ wp-content/themes/seven/page.php
@@ -25,18 +25,57 @@
                 e.g. site_url() => current page's url
                 e.g. site_url("/the-loop") => current page's url/the-loop
             -->
-            <b><a href="<?php echo site_url("/the-loop") ?>">The Loop</a></b>
-            <a href="">LinkedIn Learning</a>
-            <a href="">URLs SEO</a>
+            
+            <?php
+            /*
+            get_pages();
+            this function returns the list of pages in memory
+            it is similar to wp_list_pages();
+            */
+                $is_a_parent = get_pages(array( // if the current page has children, this function will return a collection of any and all children pages
+                    "child-of" => get_the_ID()
+                ));
+                if(get_permalink(wp_get_post_parent_id(get_the_ID())) || $is_a_parent){ ?> <!-- if the current page has a parent or if it is a parent -->
+                    <p><b><i>side navigation</i></b></p>
+                    <b><a href="<?php echo get_permalink(wp_get_post_parent_id(get_the_ID())) ?>"><?php echo get_the_title(wp_get_post_parent_id(get_the_ID())) ?></a></b>
+                    <?php
+                    /*
+                    wp_list_pages();
+                    this will give you the links to every single page on our website
+
+                    this function takes an associative array as an argument
+                    associative array:
+                    $animalSounds = array(
+                        "cat" => "meow",
+                        "dog" => "bark",
+                        "pig" => "oink"
+                    );
+                    $animalSound["dog"]; to access the value
+                    */
+                    $parent_page_id = wp_get_post_parent_id(get_the_ID());
+                    if($parent_page_id){ // if it is the parent page
+                        $findChildOf = $parent_page_id;
+                    } else{ // if it is the child page
+                        $findChildOf = get_the_ID();
+                    }
+
+                    wp_list_pages(array(
+                        "title_li" => NULL,
+                        "child_of" => $findChildOf,
+                        "sort_column" => "menu_order"
+                    ));
+                    ?>
+                <?php }
+            ?>
         </nav>
         <p><?php the_content() ?></p>
         <?php 
                 /*
-                get_the_ID()
+                get_the_ID();
                 this wordpress method will give us the id of the page that
                 we are currently viewing
 
-                wp_get_post_parent_id()
+                wp_get_post_parent_id();
                 this wordpress method will give us the id of the parent page
                 of the page that we are viewing currently
 
@@ -61,7 +100,10 @@
                             get_permalink();
                             instead of being limited to just the current page or post that we have looped through
                             we can pass it an id, and it will give us the permalink for that post or page
-                         -->
+                         
+                            generally in wordpress functions, get_...(); will return the value so you need to echo it out
+                            while the normal functions without the get keyword will echo out by default like the_title();
+                        -->
                         <a href="<?php echo get_permalink($parentPageId); ?>">Back to <?php echo get_the_title($parentPageId); ?></a>
                         <p>You are on the page: <?php the_title(); ?></p>
                     </div>
